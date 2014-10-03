c91dc493c936a25705caf483720905d81814a686
Pinaet Poonsarakhun <pinaet@gmail.com>
2014-10-03 13:33:01 +0700

    add iframe to the website

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -9,7 +9,7 @@ $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
    // animate
    $('html, body').animate({
        scrollTop: $(this.hash).offset().top -50
-     }, 700, function(){
+     }, 500, function(){
 
        // when done, add hash to url
        // (default click behaviour)
@@ -29,7 +29,27 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
    // animate
    $('html, body').animate({
        scrollTop: $(this.hash).offset().top -50
-     }, 700, function(){
+     }, 500, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+$("#back-to-top a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50
+     }, 500, function(){
 
        // when done, add hash to url
        // (default click behaviour)
